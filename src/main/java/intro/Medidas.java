19dab5e06da032d180be6a4af0855085672bb093
Gabriela Oliveira <gabriela.of@hotmail.com>
2021-07-16 10:30:50 -0300

    Inclui o "Bom Dia!!!" no começo da impressão de texto no Console

---
+++ src/main/java/intro/Medidas.java
@@ -0,0 +1,45 @@
+// 1 - Pacote
+package intro;
+
+// 2 - Referência as bibliotecas
+
+// 3 - Classe
+public class Medidas {
+    // 3.1 - Atributos - Características
+
+    // 3.2 - Métodos e Funções
+    public static void main(String[] args){
+        System.out.println("Bom Dia!!!");
+        System.out.println("Cálculo de Areas");
+
+        // Calculo de área - Reduzido
+        int largura = 4;
+        int comprimento = 3;
+        System.out.println("Para a largura de " + largura + "m e o comprimento de "
+                + comprimento + "m a área é de "  + largura * comprimento + "m²");
+
+
+
+    }
+    public void calcularAreaModoExtenso(){
+        //Calculo de área - exemplo: o tamanho do tapete ou do piso
+        int largura;
+        int comprimento;
+        int resultado;
+
+        largura = 5; // Largura recebe 4
+        comprimento = 6;
+
+        resultado = largura * comprimento;
+        System.out.println("Para a largura de " + largura + "m e o comprimento de "
+                + comprimento + "m a área é de "  + resultado + "m²");
+
+    }
+    public void calcularAreaModoCompacto() {
+        // Calculo de área - Reduzido
+        int largura = 4;
+        int comprimento = 3;
+        System.out.println("Para a largura de " + largura + "m e o comprimento de "
+                + comprimento + "m a área é de "  + largura * comprimento + "m²");
+    }
+}
